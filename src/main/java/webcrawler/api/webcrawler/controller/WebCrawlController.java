1733d4902197e4b5ccade1e9bd3cb22e76708906
Ishan Vimukthi <roxishan8@gmail.com>
2022-09-25 13:24:37 +0530

    yml config added for swagger

--- src/main/java/webcrawler/api/webcrawler/controller/WebCrawlController.java
+++ src/main/java/webcrawler/api/webcrawler/controller/WebCrawlController.java
@@ -7,8 +7,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import web_crawler.api.webcrawler.dto.UrlListDto;
-import web_crawler.api.webcrawler.service.WebCrawlerService;
+import webcrawler.api.webcrawler.dto.UrlListDto;
+import webcrawler.api.webcrawler.service.WebCrawlerService;
 
 @RestController
 @RequestMapping(value = "/search")
